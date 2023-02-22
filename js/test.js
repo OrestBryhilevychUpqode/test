06a47b0dd38501e4e4ba74bb3f6a50f9b13fcf67
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2023-02-22 20:32:04 +0200

    fix

--- js/test.js
+++ js/test.js
@@ -1,62 +1,62 @@
-;(() => {
-  const modal = document.querySelector(".modal")
-  const bg = document.querySelector(".bg")
-  const form = document.querySelector(".form")
-  const html = document.querySelector("html")
+(() => {
+    const modal = document.querySelector('.modal');
+    const bg = document.querySelector('.bg');
+    const form = document.querySelector('.form');
+    const html = document.querySelector('html');
 
-  bg.addEventListener("click", () => {
-    console.log("bg")
-    modal.style.display = "none"
-    html.classList.remove("no-scroll")
-  })
+    bg.addEventListener('click', () => {
+        console.log('bg');
+        modal.style.display = 'none';
+        html.classList.remove('no-scroll');
+    });
 
-  form.addEventListener("click", () => {
-    console.log("form")
-  })
+    form.addEventListener('click', () => {
+        console.log('form');
+    });
 
-  const tabs = document.querySelectorAll(".js-tab-item")
+    const tabs = document.querySelectorAll('.js-tab-item');
 
-  for (const tab of tabs) {
-    tab.addEventListener("click", (e) => {
-      removeActive()
-      e.currentTarget.classList.add("active")
-      modal.style.display = "flex"
-      html.classList.add("no-scroll")
-    })
-  }
-
-  function removeActive() {
-    const newTabs = document.querySelectorAll(".js-tab-item")
-    for (const tab of newTabs) {
-      tab.classList.remove("active")
+    for (const tab of tabs) {
+        tab.addEventListener('click', e => {
+            removeActive();
+            e.currentTarget.classList.add('active');
+            modal.style.display = 'flex';
+            html.classList.add('no-scroll');
+        });
     }
-  }
 
-  // ------------------------
-  function updateWidth() {
-    document.getElementById("window-width").innerHTML =
-      document.documentElement.clientWidth
-  }
+    function removeActive() {
+        const newTabs = document.querySelectorAll('.js-tab-item');
+        for (const tab of newTabs) {
+            tab.classList.remove('active');
+        }
+    }
 
-  window.addEventListener("resize", updateWidth)
-  updateWidth()
+    // ------------------------
+    function updateWidth() {
+        document.getElementById('window-width').innerHTML =
+            document.documentElement.clientWidth;
+    }
 
-  try {
-    var mp3 =
-      "data:audio/mpeg;base64,/+MYxAAAAANIAUAAAASEEB/jwOFM/0MM/90b/+RhST//w4NFwOjf///PZu////9lns5GFDv//l9GlUIEEIAAAgIg8Ir/JGq3/+MYxDsLIj5QMYcoAP0dv9HIjUcH//yYSg+CIbkGP//8w0bLVjUP///3Z0x5QCAv/yLjwtGKTEFNRTMuOTeqqqqqqqqqqqqq/+MYxEkNmdJkUYc4AKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
 
-    var audio = new Audio()
+    try {
+        var mp3 =
+            'data:audio/mpeg;base64,/+MYxAAAAANIAUAAAASEEB/jwOFM/0MM/90b/+RhST//w4NFwOjf///PZu////9lns5GFDv//l9GlUIEEIAAAgIg8Ir/JGq3/+MYxDsLIj5QMYcoAP0dv9HIjUcH//yYSg+CIbkGP//8w0bLVjUP///3Z0x5QCAv/yLjwtGKTEFNRTMuOTeqqqqqqqqqqqqq/+MYxEkNmdJkUYc4AKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq';
 
-    audio.autoplay = true
-    audio.volume = 0
+        var audio = new Audio();
 
-    audio.addEventListener("play", function () {
-      document.querySelector("html").classList.add("autoplay")
-      document.querySelector("html").classList.remove("no-autoplay")
-    })
+        audio.autoplay = true;
+        audio.volume = 0;
 
-    audio.src = mp3
-  } catch (e) {
-    document.getElementById("autoplay").innerHTML = e
-  }
-})()
+        audio.addEventListener('play', function () {
+            document.querySelector('html').classList.add('autoplay');
+            document.querySelector('html').classList.remove('no-autoplay');
+        });
+
+        audio.src = mp3;
+    } catch (e) {
+        document.getElementById('autoplay').innerHTML = e;
+    }
+})();
