c0029d1aec0f256dab3937d7317dc9a372b3c38c
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2023-03-05 12:55:19 +0200

    fix

--- js/aedifon.js
+++ js/aedifon.js
@@ -40,3 +40,70 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+// --------------------
+
+const scroll = document.querySelector('.btn').getBoundingClientRect().top;
+console.log(scroll);
+
+window.addEventListener('scroll', () => {
+    const scroll = document
+        .querySelector('.aed-test')
+        .getBoundingClientRect().top;
+    console.log(scroll);
+});
+
+// $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
+
+//     if (scroll >= 500) {
+//         $('.clearHeader').addClass('darkHeader');
+//     } else {
+//         $('.clearHeader').removeClass('darkHeader');
+//     }
+// });
+
+// підказка
+
+function addToolTip() {
+    let tooltipElem;
+
+    document.onmouseover = function (event) {
+        let target = event.target;
+
+        // якщо у нас є HTML підказка...
+        let tooltipHtml = target.dataset.tooltip;
+        if (!tooltipHtml) return;
+
+        // ...створіть елемент підказки
+
+        tooltipElem = document.createElement('div');
+        tooltipElem.className = 'tooltip';
+        tooltipElem.innerHTML = tooltipHtml;
+        document.body.append(tooltipElem);
+
+        // розташуйте його над анотованим елементом (угорі по центру)
+        let coords = target.getBoundingClientRect();
+
+        let left =
+            coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+        if (left < 0) left = 0; // не перетинайте лівий край вікна
+
+        let top = coords.top - tooltipElem.offsetHeight - 25;
+        if (top < 0) {
+            // якщо перетинає верхній край вікна, розташуйте знизу
+            top = coords.top + target.offsetHeight + 5;
+        }
+
+        tooltipElem.style.left = left + 'px';
+        tooltipElem.style.top = top + 'px';
+    };
+
+    document.onmouseout = function (e) {
+        if (tooltipElem) {
+            tooltipElem.remove();
+            tooltipElem = null;
+        }
+    };
+}
+
+addToolTip();
